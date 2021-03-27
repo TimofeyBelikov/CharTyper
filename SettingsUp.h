6c5ae9034cf35851fd10f93bb8a9cfcdfa7c8e22
timofeyBelikov <belikov-tima@mail.ru>
2021-03-27 03:19:10 +0300

    Добавлен основной класс

--- SettingsUp.h
+++ SettingsUp.h
@@ -1,5 +1,9 @@
 #ifndef SETTINGSUP_H
 #define SETTINGSUP_H
+#include <Windows.h>
+#include <iostream>
 
+void SetColor();
+void setUpFont();
 
 #endif
---
+++ Typer.h
@@ -0,0 +1,11 @@
+#ifndef TYPER_H
+#define TYPER_H
+class Typer {
+private:
+
+public:
+	
+
+};
+
+#endif // !TYPER_H
