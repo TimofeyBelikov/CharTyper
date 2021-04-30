f14acfa8913e352fda2fc3ad09492bf9b3b7d417
TimofeyBelikov <55021882+TimofeyBelikov@users.noreply.github.com>
2021-05-01 00:19:34 +0300

    Update SettingsUp.cpp

--- SettingsUp.cpp
+++ SettingsUp.cpp
@@ -1,8 +1,28 @@
 #include "SettingsUp.h"
-void SetColor() {
-	//функция для настройки цвета
+extern HANDLE cConsole;
+
+void SetColor(int text, int bg) {
+	SetConsoleTextAttribute(cConsole, (WORD)((bg << 4) | text));
+	return;
 }
 
-void setUpFont() {
-	//функция для настройки шрифта
+void setUpFont(CONSOLE_FONT_INFOEX* cfi, CONSOLE_CURSOR_INFO* cursor) {
+	SetConsoleDisplayMode(cConsole, CONSOLE_FULLSCREEN_MODE, (PCOORD)(0, 0));
+	cursor->bVisible = false;
+	cursor->dwSize = 99;
+	SetConsoleCursorInfo(cConsole, cursor);
+
+	cfi->cbSize = sizeof(CONSOLE_FONT_INFOEX);
+	cfi->nFont = 0;
+	cfi->dwFontSize.X = 14;
+	cfi->dwFontSize.Y = 25;
+
+	cfi->FontFamily = FF_DONTCARE;
+	cfi->FontWeight = 400;
+	SetCurrentConsoleFontEx(cConsole, FALSE, cfi);
+	SetConsoleCP(1251);         // СѓСЃС‚Р°РЅРѕРІРєР° РєРѕРґРѕРІРѕР№ СЃС‚СЂР°РЅРёС†С‹ win-cp 1251 РІ РїРѕС‚РѕРє РІРІРѕРґР°
+	SetConsoleOutputCP(1251);
+	system("mode con cols=96 lines=25");
+	setlocale(LC_CTYPE, "rus");
+	return;
 }
